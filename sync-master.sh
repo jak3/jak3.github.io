bb8532781c08ca6bff70878844abfdaff75485a7
Giacomo <jake@xmg.digar.fc>
2017-05-06 07:33:41 +0200

    add sync-master script (rsync oneline wrapper)

---
+++ sync-master.sh
@@ -0,0 +1,7 @@
+rsync -a --filter='P _site/'      \
+         --filter='P _cache/'     \
+         --filter='P .git/'       \
+         --filter='P .gitignore'  \
+         --filter='P .stack-work' \
+         --delete-excluded        \
+         _site/ .
