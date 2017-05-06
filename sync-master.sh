0c5b35d4c06b52bfda5e6831cb851f90995db413
Giacomo <jake@xmg.digar.fc>
2017-05-06 07:41:47 +0200

    fix typo, ATM solo italiano

--- sync-master.sh
+++
@@ -1,7 +0,0 @@
-rsync -a --filter='P _site/'      \
-         --filter='P _cache/'     \
-         --filter='P .git/'       \
-         --filter='P .gitignore'  \
-         --filter='P .stack-work' \
-         --delete-excluded        \
-         _site/ .
